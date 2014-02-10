acbb3c545b894c5792fa0aaa661ad6e41351129f
mmoven12 <mmoven12@gmail.com>
2014-02-10 20:50:22 +0800

    I changed asd. m
    asd

--- asd.m
+++ asd.m
@@ -1,19 +1,2 @@
-function ColorMap = getLivingroomMap
-ColorMap = {%255,255,255; ...% 0 background
-    'tvset',        0,128,0; ...          % 1 tvset
-    'tvcabinet',    128,0,0;...           % 2 tvcabinet
-    'sofa',         0,0,64;...            % 3 sofa
-    'table',        0,128,64;...          % 4 table
-    'chair',        128,64,0;...          % 5 chair
-    'lamp',         64,0,0;...            % 6 lamp
-    'teatable',     128,0,128;...         % 7 teatable
-    'cabinet',      0,0,128;...           % 8 cabinet
-    'airconditioner', 64,128,0;...        % 9 airconditioner
-    'vase',         192,0,192;...         % 10 vase
-    'lampcabinet'   0,0,192;...           % 11 lamp cabinet  
-    'doorwindow', 	0,192,0;...           % 12 doorwindow 
-    'curtain',      192,0,0;...           % 13 plant 
-    'ceiling', 	    64,64,64;...          % 14 ceiling 
-    'floor',        128,128,128;...       % 15 floor 
-    'wall',         192,192,192};...      % 16 wall 
-end
+cao ni ma de chen xiao wu
+wo yi hou yi ding yao hun de bi ni hao
